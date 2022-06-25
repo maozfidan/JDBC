292ecbdd2eb767caed312304967d9837ab05508c
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-06-25 15:58:45 +0200

    25.06.2022

--- src/test/java/Query03.java
+++ src/test/java/Query03.java
@@ -21,7 +21,7 @@ public class Query03 {
             System.out.printf("%-6d %-15.15s %-8s\n", veri.getInt(1), veri.getString(2), veri.getString(3));
         }
 
-        // SORU2:SATIS ve MUHASEBE bolumlerinde calişan personelin isimlerini ve maaşlarını,
+        // SORU2: SATIS ve MUHASEBE bolumlerinde calişan personelin isimlerini ve maaşlarını,
         // maaş ters sıralı listeleyiniz.
 
         System.out.println("==========================================================================");
@@ -36,5 +36,44 @@ public class Query03 {
                     veri2.getInt(2));
         }
 
+        // SORU3: Tüm bölümlerde çalışan personel isimlerini, bölüm isimlerini ve maaşlarını,
+        // bölüm ve maas sıralı listeleyiniz.
+        // NOT: Çalışanı olamasa bile bölüm ismi gösterilmelidir.
+
+        System.out.println("==========================================================================");
+
+
+
+        ResultSet veri3 = st.executeQuery("select bolum_isim, personel_isim, maas " +
+                "from bolumler as B left join personel as p on B.bolum_id = P.bolum_id " +
+                "order by bolum_isim desc, P.maas;");
+
+        while (veri3.next()){
+            //System.out.println(veri3.getString(1) + veri3.getString(2) + veri3.getInt(3));
+
+            System.out.printf("%-10s %-10s %6d\n",veri3.getString(1),veri3.getString(2),veri3.getInt(3));
+        }
+
+
+        // SORU4: Maaşı en yüksek 10 kişinin bolümünü, adını ve maaşını listeleyiniz.
+
+        System.out.println("==========================================================================");
+
+        ResultSet veri4 = st.executeQuery("select p.personel_isim, b.bolum_isim, p.maas " +
+                "from bolumler b left join personel p on b.bolum_id=p.bolum_id order by maas " +
+                "desc limit 10");
+
+        while (veri4.next()){
+
+            System.out.printf("%-10s %-10s %6d\n", veri4.getString(1), veri4.getString(2), veri4.getInt(3));
+
+
+        }
+
+
+
+        con.close();
+        st.close();
+        veri.close();
     }
 }
